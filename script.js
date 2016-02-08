5abee5b905e551902f0d9385b6f55c2a82287825
Annie Jiao <anniej@mit.edu>
2016-02-08 13:20:35 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,75 +1,230 @@
-/* 
- * Lazy Line Painter - Path Object 
- * Generated using 'SVG to Lazy Line Converter'
- * 
- * http://lazylinepainter.info 
- * Copyright 2013, Cam O'Connell  
- *  
- */ 
- 
-var pathObj = {
-    "undefined": {
-        "strokepath": [
+var canvas, stage, carve;
+var ctx, ctxcarve;
+var side;
+var drawingCanvas;
+var oldPt;
+var oldMidPt;
+var title;
+var color;
+var stroke;
+var index;
+var jsonColors = [];
+var savecircle = null;
+var IMG_DATA = null;
 
-            {
-                "path": "M402.934,101.269c21.264-7.862,27.339-26.624,16.261-38.416c-11.078-11.795-39.848-18.944-65.937-3.577    c-25.786,15.189-29.683,47.592-21.199,63.927c-4.533,4.069-9.326,6.007-12.574,6.121c-2.805,0.097-3.883-1.695-4.278-3.358    c-0.314-1.306-0.2-2.684,0.229-3.958l15.499-46.385c3.744-11.203-4.593-22.777-16.402-22.777c-9.812,0-18.541,6.21-21.756,15.479    c-3.823,11.024-8.498,25.625-10.279,35.804c-0.871,4.993-1.288,10.257-0.992,15.285c-5.41,6.667-12.027,9.769-16.152,9.911    c-2.81,0.097-3.884-1.695-4.283-3.358c-0.311-1.306-0.196-2.684,0.232-3.958l14.611-43.338h8.357l9.091-25.825h-8.744    l8.073-23.944h-22.513c-7.863,20.907-33.238,23.944-33.238,23.944l-8.65,25.825h6.888c-2.838,8.809-5.554,18.245-6.813,25.458    c-0.746,4.265-1.156,8.733-1.077,13.08c-5.39,6.756-11.606,11.991-15.183,12.116c-5.182,0.179-4.468-6.078-4.468-6.078    l23.587-70.401h-40.205c0,0-1.877,4.861-4.365,11.881c-2.345-6.438-7.638-14.025-19.579-14.025    c-20.729,0-38.598,21.442-45.029,36.989c-2.773,6.695-5.636,18.197-5.332,29.126l-3.591,7.962c-0.66,1.46-2.116,2.402-3.722,2.402    c-2.812,0-4.782-2.776-3.847-5.433c3.226-9.161,9.611-27.359,11.845-34.058c3.037-9.112,6.61-36.989-19.476-36.989    c-17.595,0-29.248,11.21-34.976,18.512c-1.777-9.79-7.801-18.512-23.637-18.512C1.428,50.701,0,84.651,0,84.651l15.364,0.357    l3.251-9.936c0.635-1.353,1.991-2.216,3.483-2.216c2.651,0,4.511,2.616,3.64,5.118L3.573,141.73l44.147-5.133l12.809-40.571    c0,0,1.428-4.354,2.327-10.361c3.733-4.688,8.647-10.075,11.299-9.768c4.643,0.536,3.572,5.179,3.572,5.179    s-5.896,17.155-10.364,31.093c-4.468,13.937-1.07,32.52,23.766,32.52c16.235,0,26.057-5.343,30.911-9.043    c3.883,5.449,10.197,9.043,20.015,9.043c14.726,0,25.143-11.135,32.195-23.594c1.199,12.787,7.406,23.594,23.377,23.594    c15.243,0,26.717-7.373,32.685-12.299c3.318,7.263,9.664,12.299,20.564,12.299c15.81,0,27.573-7.938,33.333-12.841    c3.248,7.552,9.633,12.841,20.804,12.841c15.836,0,27.609-7.962,33.359-12.862c14.868,15.317,45.539,23.552,76.429-8.684    c1.963-2.048,1.845-5.32-0.261-7.22c-1.931-1.741-4.879-1.738-6.781,0.036c-5.343,4.975-16.795,13.9-30.2,13.9    c-10.382,0-12.556-10.935-10.789-23.08C373.329,107.129,387.466,106.986,402.934,101.269 M159.568,120.388    c-6.432-2.858,1.07-21.086,4.107-30.197c3.041-9.114,7.149-20.371,15.368-18.405c2.516,0.664,3.426,2.556,3.644,4.543    c-0.999,3.044-1.999,6.195-2.94,9.336l-10.889,28.47C168.857,114.135,165.999,123.246,159.568,120.388 M395.429,78.67    c-1.503,7.099-11.453,14.747-25.312,14.683c1.724-4.932,3.865-9.446,6.11-12.812C385.778,66.247,397.038,71.083,395.429,78.67",
-                "duration": 2400
-            },
-            {
-                "path": "M325.562,42.196c11.652,0,21.1-9.447,21.1-21.1S337.214,0,325.562,0s-21.1,9.444-21.1,21.096    S313.909,42.196,325.562,42.196",
-                "duration": 600
-            },
-        ],
-        "dimensions": {
-            "width": 437,
-            "height": 157
-        }
+var carveCanvas = null;
+
+function init() {
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext('2d');
+
+    carve = document.getElementById('carveout');
+    ctxcarve = carve.getContext("2d");
+
+    var w = window.innerWidth || e.clientWidth || g.clientWidth;
+    var h = window.innerHeight || e.clientHeight || g.clientHeight;
+    side = Math.min(w, h);
+
+    // $("#myCanvas")[0].attr("width", width).attr("height", height);
+
+    ctx.canvas.height = side;
+    ctx.canvas.width = side;
+
+    index = 0;
+    colors = ["#B03060", "#FE9A76", "#FFD700", "#32CD32", "#016936", "#008080", "#0E6EB8", "#EE82EE", "#B413EC", "#FF1493", "#A52A2A", "#A0A0A0", "#f2f2f2", "#000000"];
+
+    color = colors[0];
+
+    //check to see if we are running in a browser with touch support
+    stage = new createjs.Stage(canvas);
+    stage.autoClear = false;
+    stage.enableDOMEvents(true);
+
+    createjs.Touch.enable(stage);
+    createjs.Ticker.setFPS(24);
+
+    drawingCanvas = new createjs.Shape();
+
+    stage.addEventListener("stagemousedown", handleMouseDown);
+    stage.addEventListener("stagemouseup", handleMouseUp);
+    stage.addChild(drawingCanvas);
+    stage.update();
+
+    make_carve("carve0.png");
+    make_palette();
+    make_bgPicker().done(function() {
+        $("#blank").css("height", document.getElementById('img0').getBoundingClientRect().height)
+    })
+
+    $("#save").on("click", save_img)
+    $("#blank").on("click", function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctxcarve.clearRect(0, 0, carve.width, carve.height);
+    })
+
+    $('img').on('click', function(event) {
+        var src = $(this).attr('src');
+        make_carve(src);
+    });
+
+}
+
+function handleMouseDown(event) {
+    if (!event.primary) {
+        return;
     }
-}; 
- 
- 
-/* 
- Setup and Paint your lazyline! 
- */ 
- 
- $(document).ready(function(){ 
-   $('#undefined').lazylinepainter( 
-   {
-      "svgData": pathObj,
-      "strokeWidth": 2,
-      "strokeColor": "#e09b99"
-  }).lazylinepainter('paint'); 
- });
+    if (stage.contains(title)) {
+        stage.clear();
+        stage.removeChild(title);
+    }
 
- $("#carve").onload=function(){
-  console.log("onload")
-  var w = $('#carve').clientWidth;
-  var h = $('#carve').clientHeight;
-  console.log(w,h);
-  $('#undefined').width(w);
-  $('#undefined').css("height", h);
- }
+    stroke = 40;
+    oldPt = new createjs.Point(stage.mouseX, stage.mouseY);
+    oldMidPt = oldPt.clone();
+    stage.addEventListener("stagemousemove", handleMouseMove);
+}
 
- /* 
- Setup and Paint your lazyline! 
- */ 
- 
- $(document).ready(function(){ 
- $('#undefined').lazylinepainter( 
- {
-    "svgData": pathObj,
-    "strokeWidth": 2,
-    "strokeColor": "#e09b99"
-}).lazylinepainter('paint'); 
- });/* 
- Setup and Paint your lazyline! 
- */ 
- 
- $(document).ready(function(){ 
- $('#undefined').lazylinepainter( 
- {
-    "svgData": pathObj,
-    "strokeWidth": 2,
-    "strokeColor": "#e09b99"
-}).lazylinepainter('paint'); 
- });
+function handleMouseMove(event) {
+    if (!event.primary) {
+        return;
+    }
+    var midPt = new createjs.Point(oldPt.x + stage.mouseX >> 1, oldPt.y + stage.mouseY >> 1);
+    drawingCanvas.graphics.clear()
+        .setStrokeStyle(stroke, 'round', 'round')
+        .beginStroke(color).moveTo(midPt.x, midPt.y)
+        .curveTo(oldPt.x, oldPt.y, oldMidPt.x, oldMidPt.y);
+
+    oldPt.x = stage.mouseX;
+    oldPt.y = stage.mouseY;
+    oldMidPt.x = midPt.x;
+    oldMidPt.y = midPt.y;
+
+    stage.update();
+}
+
+function handleMouseUp(event) {
+    if (!event.primary) {
+        return;
+    }
+    stage.removeEventListener("stagemousemove", handleMouseMove);
+}
+
+function make_palette() {
+    for (var i = 0; i < colors.length; i++) {
+        jsonColors.push({
+            "i": i,
+            "color": colors[i]
+        })
+    }
+    var svgContainer = d3.select("body").append("svg")
+        .attr("width", 200)
+        .attr("height", 400)
+        .attr("id", "palette");
+
+    var circles = svgContainer.selectAll("circle")
+        .data(jsonColors)
+        .enter()
+        .append("circle");
+
+    var circleAttributes = circles
+        .attr("cx", function(d) {
+            return d.i % 2 * 45 + 40
+        })
+        .attr("cy", function(d) {
+            return ~~(d.i / 2) * 45 + 40
+        })
+        .attr("r", function(d) {
+            return 18
+        })
+        .style("fill", function(d) {
+            return d.color;
+        })
+        .filter(function(d) {
+            return d.i == 0
+        })
+        .style("stroke", "#0d0d0d").style("stroke-width", 4)
+
+    circles.filter(function(d) {
+            return d.color == "#f2f2f2"
+        })
+        .style("stroke", "#0d0d0d").style("stroke-width", 4)
+
+    circles.on('click', function(d) {
+        color = d.color;
+        circles.transition().attr("r", 18).filter(function(d) {
+            return d.color != "#f2f2f2"
+        }).style("stroke", "none");
+        d3.select(this).transition().attr("r", 23)
+            .transition().attr("r", 18)
+            .style("stroke", "#0d0d0d")
+            .style("stroke-width", 4);
+    });
+
+    var savebutton = svgContainer.append("g").attr("id", "save");
+
+    savecircle = savebutton.append("circle")
+        .attr("cx", function(d) {
+            return colors.length % 2 * 45 + 40
+        })
+        .attr("cy", function(d) {
+            return ~~(colors.length / 2) * 45 + 40
+        })
+        .attr("r", function(d) {
+            return 18
+        })
+        .style("fill", "none")
+        .style("stroke", "#0d0d0d")
+        .style("stroke-width", 4);
+
+    savebutton.append("text").attr('font-family', 'FontAwesome')
+        .attr("x", colors.length % 2 * 45 + 40)
+        .attr("y", ~~(colors.length / 2) * 45 + 40)
+        .attr('text-anchor', 'middle')
+        .attr('dominant-baseline', 'central')
+        .attr('font-size', '20px')
+        .text(function(d) {
+            return '\uf063'
+        });
+}
+
+function make_carve(src) {
+    // var d = $.Deferred();
+    base_image = new Image();
+    carve.width = carve.height = side;
+
+    base_image.onload = function() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctxcarve.clearRect(0, 0, carve.width, carve.height);
+        ctxcarve.drawImage(base_image, 0, 0, side, side);
+        ctxcarve.fill();
+    }
+    base_image.src = src;
+
+    // setTimeout(function() {
+    //     d.resolve();
+    // }, 500);
+    // return d;
+}
+
+function make_bgPicker() {
+    var d = $.Deferred();
+    var n = 3;
+    var src;
+    for (var i = 0; i < n; i++) {
+        src = "carve" + i + ".png";
+        $("#bgPicker").append("<img id='img" + i + "' src='" + src + "'></img>")
+    }
+    setTimeout(function() {
+        d.resolve();
+    }, 500);
+    return d;
+}
+
+function save_img() {
+    ctx.drawImage(carve, 0, 0);
+    ctx.fill();
+
+    canvas.toBlob(function(blob) {
+        saveAs(blob, "natie painted.png");
+    });
+}
